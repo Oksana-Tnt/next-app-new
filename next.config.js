b72ffa6f58e84d76628862bc4328a556899fafc9
Oksana-Tnt <oksanatantsiura@gmail.com>
2024-03-10 21:29:05 +0100

    update next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/next-app-new",
-  output: "export",
-  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
